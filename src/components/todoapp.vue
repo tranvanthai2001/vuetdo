7792883224d6bc20bfe306e781e0f7f10123b20c
Trần Văn Thái <tranvanthai171101@gmail.com>
2023-04-30 02:07:17 +0700

    pushcontinue

---
+++ src/components/todoapp.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-5">Todo App</h2>
+    <div class="d-flex">
+      <input v-model="name" type="text" placeholder="new item" class="form-control">
+      <button @click="addTask" class="btn btn-success">Add</button>
+    </div>
+    <table class="table caption-top">
+  <caption>List of users</caption>
+  <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Status</th>
+      <th scope="col" >#</th>
+      <th scope="col" >#</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(task, index) in tasks" :key="index">
+      <th>{{ task.first_name }}</th>
+      <td>{{ task.last_name }}</td>
+      <td>
+        <div>
+          <span @click="updateTask(index)" class="fa fa-pen"></span>
+        </div>
+      </td>
+      <td>
+        <div @click="deleteTask(index)">
+          <span class="fa fa-trash"></span>
+        </div>
+      </td>
+    </tr>
+    
+  </tbody>
+</table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      name: '',
+      edit : null,
+      tasks: []
+    }
+  },
+  async mounted () {
+    try {
+      const response = await axios.get('https://reqres.in/api/users?page=1')
+      this.tasks = response.data.data
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  methods: {
+    addTask() {
+      if(this.name.length ===0) return;
+      if(this.edit ===null) {
+        this.tasks.push({
+          first_name: this.name,
+          last_name: this.name
+        });
+      }else {
+        this.tasks[this.edit].first_name = this.name,
+        this.edit = null
+      }
+      this.name =''
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
+    },
+    updateTask(index) {
+      this.name = this.tasks[index].first_name,
+        this.edit= index
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
